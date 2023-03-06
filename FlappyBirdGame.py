32ffebbdaef9b7336d3cb3792f2e4c922d19d6da
George <george-andreas@hotmail.com>
2023-03-06 03:14:55 -0800

    added fitness value

--- FlappyBirdGame.py
+++ FlappyBirdGame.py
@@ -3,83 +3,67 @@ from settings import *
 import os
 import math
 import time
-from Pipe import Pipe
-from Bird import Bird
-"""
-    Class to run Flappy bird
-"""
+from model.Pipe import Pipe
+from model.Bird import Bird
+from FlappyBirdUI import FlappyBirdUI
+
 class FlappyBirdGame:
+    """
+        Class to run Flappy bird game
+    """
     def __init__(self, screen) -> None:
         self.isRunning = True
         self.start = False
         self.gameOver = False
 
         self.screen = screen
-        self.loadImages()
+        self.UI = FlappyBirdUI(self)
 
         ## Create objects needed for Flappy bird
         self.bird = Bird(self.screen.get_width()//2, self.screen.get_height()//2)
         self.pipeList = [Pipe(self.screen.get_width())]
 
-        ## Identify the number of tiles needed for the background and ground image
-        self.backgroundTiles = math.ceil(self.screen.get_width()/ self.backgroundImg.get_width()) + 1
-        self.groundTiles = math.ceil(self.screen.get_width()/ self.groundImg.get_width()) + 1
-
         ## Scroll speed, i.e the speed in which the screen moves 
         self.scroll = 0
         self.scrollSpeed = 4 #* 60
 
-        ## Delta Time and clock Variables
+        ## Clock Variables
         self.clock = pygame.time.Clock()
-        self.now = time.time()
-        self.prevTime = self.now
-        self.dt = 0
-
-        ## Use this variable if we want to add python pipe based on time passed
-        self.pipeFrequency = 1.5 # seconds to add additional pipe
-        self.prevPipeTime = time.time() - self.pipeFrequency
 
         ## Score
         self.score = 0
-        self.scoreSize = 50
-        self.scoreFont = pygame.font.Font(os.path.join(FONTS_FOLDER, "flappy-font.ttf"), self.scoreSize)
 
     def loop(self):
-        self.getDt()
-        self.update()
+        self.getEvent()
+        self.update(self.events)
         self.draw()
 
-    def getDt(self):
-        currentTime = time.time()
-        self.dt = currentTime - self.prevTime
-        self.prevTime = currentTime
-        
-        self.dt = 1
-
-    def update(self):
-        for event in pygame.event.get():
+    def getEvent(self):
+        self.events = pygame.event.get()
+        for event in self.events:
             if event.type == pygame.QUIT:
                 self.isRunning = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.start = True
+                self.bird.jump()
 
+    def update(self, events):
         if self.start:
-            self.scroll -= self.scrollSpeed * self.dt
-            if abs(self.scroll) > self.backgroundImg.get_width():
-                self.scroll = 0
+            self.updateScroll()
             self.bird.update()
             self.updatePipes()
 
+            if self.bird.y + self.bird.height > BACKGROUND_HEIGHT or self.bird.y < 0:
+                self.reset()
+
+    def updateScroll(self):
+        self.scroll -= self.scrollSpeed 
+        if abs(self.scroll) > self.UI.backgroundImg.get_width():
+            self.scroll = 0
+
     def updatePipes(self):
-        now = time.time()
-
-        #Use this if we want to add pipe based on every time passed
-        # if now - self.prevPipeTime >= self.pipeFrequency:
-        #     self.pipeList.append(Pipe(self.screen.get_width()))
-        #     self.prevPipeTime = now
-
         for pipe in self.pipeList:
-            pipe.update(self.scrollSpeed * self.dt)
+            pipe.update(self.scrollSpeed)
 
             if pipe.collideBird(self.bird):
                 self.reset()
@@ -90,61 +74,16 @@ class FlappyBirdGame:
                 self.pipeList.append(Pipe(self.screen.get_width()))
                 self.score += 1
             
-            # if pipe.x + pipe.width < self.screen.get_width() // 2:
-            #     self.pipeList.append(Pipe(self.screen.get_width()))
-
             if pipe.x + pipe.width < 0:
                 self.pipeList.remove(pipe)
             
     def draw(self):
-        # Draw the background
-        for i in range(self.backgroundTiles):
-            self.screen.blit(self.backgroundImg, (i * self.backgroundImg.get_width() + self.scroll, 0))
-        
-        # Draw the objects
-        self.bird.draw(self.screen)
-        self.drawPipes(self.pipeList)
-
-        # draw the ground
-        for i in range(self.groundTiles):
-            self.screen.blit(self.groundImg, 
-                             (i * self.groundImg.get_width() + self.scroll,
-                              self.backgroundImg.get_height()))
-        
-
-
-
-        scoreObj = self.scoreFont.render(str(self.score), 1, (255,255,255)).convert_alpha()       
-        self.screen.blit(scoreObj, 
-                         (self.screen.get_width()//2 - scoreObj.get_width()//2, SCREEN_HEIGHT*0.1 - scoreObj.get_height()//2),
-                         )
-        
-        # mask = pygame.mask.from_surface(scoreObj)
-        # self.screen.blit(mask.to_surface(unsetcolor=(255,255,255,255), setcolor=(0,0,0,0)), 
-        #                  (self.screen.get_width()//2 - scoreObj.get_width()//2, SCREEN_HEIGHT*0.1 - scoreObj.get_height()//2))
-                         
-        pygame.display.update()
-    
-    def drawPipes(self, pipeList):
-        for pipe in pipeList:
-            pipe.draw(self.screen)
+        self.UI.draw()
     
     def run(self):
         while self.isRunning:
             self.loop()
             self.clock.tick(60)
-
-    def loadImages(self):
-        self.backgroundImg = pygame.image.load(os.path.join(IMAGES_FOLDER, "bg1.png")).convert_alpha()
-        self.backgroundImg = pygame.transform.smoothscale(self.backgroundImg, (self.backgroundImg.get_width(), BACKGROUND_HEIGHT))
-
-        self.groundImg = pygame.image.load(os.path.join(IMAGES_FOLDER, "ground.png")).convert_alpha()
-        self.groundImg = pygame.transform.smoothscale(self.groundImg, (self.groundImg.get_width(), GROUND_HEIGHT))
     
     def reset(self):
         self.__init__(self.screen)
-
-    
-    """
-        Configure neat
-    """
---
+++ FlappyBirdGameAI.py
@@ -0,0 +1,201 @@
+from FlappyBirdGame import FlappyBirdGame
+import pygame
+import neat
+import pickle
+from model.Bird import Bird
+import os
+from settings import *
+from FlappyBirdUIAI import FlappyBirdUIAI
+from model.Pipe import Pipe
+
+class FlappyBirdGameAI(FlappyBirdGame):
+    """
+        Class to train and test an AI for Flappy bird game using NEAT
+    """
+    def __init__(self, screen) -> None:
+        FlappyBirdGame.__init__(self, screen)
+        self.isRunning = True
+        self.game = self
+        self.UI = FlappyBirdUIAI(self)
+
+        self.toDraw = True
+        self.generation = -1
+        self.pipeInd = 0
+        self.start = True
+
+        # self.nets = []
+        # self.birdList = []
+        # self.genomes = []
+        self.isTraining = True
+
+        self.setUpNeatConfig()
+
+    def run(self, genomes, config):
+        self.generation += 1
+        ## Add initial configuration to the game
+        for id, genome in genomes:
+            genome.fitness = 0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            self.nets.append(net)
+            self.birdList.append(Bird(self.screen.get_width()//2, self.screen.get_height()//2))
+            self.genomes.append(genome)
+        
+        self.score = 0
+        self.reset()
+        while self.isRunning and len(self.birdList) > 0:
+            self.loop()
+    
+    ## Override method
+    def reset(self):
+        gen = self.generation
+        toDraw = self.toDraw
+        isRunning = self.isRunning
+
+        self.__init__(self.screen)
+
+        self.generation = gen
+        self.toDraw = toDraw
+        self.start = True
+        self.isRunning = isRunning
+
+    def loop(self):
+        self.update()
+        if self.toDraw: self.draw()
+
+    def removeOutOfBoundBirds(self):
+        for bird in self.birdList:
+            if bird.y + bird.height > BACKGROUND_HEIGHT or bird.y < 0:
+                self.nets.pop(self.birdList.index(bird))
+                self.genomes.pop(self.birdList.index(bird))
+                self.birdList.pop(self.birdList.index(bird))
+    
+    def update(self):
+        self.getEvents()
+        self.updateScroll()
+
+        self.pipeInd = self.getClosestPipeIndex()
+        self.updateBird()
+        self.updatePipes()
+        self.removeOutOfBoundBirds()
+
+        if self.isTraining and self.score > 200:
+            self.isRunning = False
+
+    ## Override Method
+    def updatePipes(self):
+        for pipe in self.pipeList:
+            pipe.update(self.scrollSpeed)
+
+            for bird in self.birdList:
+                if pipe.collideBird(bird):
+                    if self.isTraining:
+                        self.genomes[self.birdList.index(bird)].fitness -=1
+                        self.genomes.pop(self.birdList.index(bird))
+                    self.nets.pop(self.birdList.index(bird))
+                    self.birdList.pop(self.birdList.index(bird))
+            
+            if not pipe.passed and self.bird.x + self.bird.width > pipe.x and self.bird.x + self.bird.width < pipe.x + pipe.width:
+                pipe.passed = True
+                if self.isTraining:
+                    for genome in self.genomes:
+                        genome.fitness += 5
+                self.pipeList.append(Pipe(self.screen.get_width()))
+                self.score += 1
+
+            if pipe.x + pipe.width < 0:
+                self.pipeList.remove(pipe)
+
+    def updateBird(self):
+        ## Reward bird for each frame it stays alive
+        for x, bird in enumerate(self.birdList): 
+            if self.isTraining:
+                if self.genomes != None:
+                    self.genomes[x].fitness += 0.1
+            bird.update()
+
+            deltaX = bird.x + bird.width - self.pipeList[self.pipeInd].x 
+            deltaYTop = bird.y - self.pipeList[self.pipeInd].height
+            deltaYBottom =  bird.y - self.pipeList[self.pipeInd].bottom
+            output = self.nets[self.birdList.index(bird)].activate((deltaX, deltaYTop, deltaYBottom))
+            if output[0] >= 0.5:
+                bird.jump()
+
+    ##Overriding the method
+    def draw(self):
+        self.UI.draw()
+    
+    ##Overriding the method
+    def getEvents(self):
+        self.events = pygame.event.get()
+        for event in self.events:
+            if event.type == pygame.QUIT:
+                self.isRunning = False
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.toDraw = not self.toDraw
+    
+    def removeOutOfBoundBirds(self):
+        for bird in self.birdList:
+            if bird.y + bird.height > BACKGROUND_HEIGHT or bird.y < 0:
+                self.nets.pop(self.birdList.index(bird))
+                self.genomes.pop(self.birdList.index(bird))
+                self.birdList.pop(self.birdList.index(bird))
+    
+    def getClosestPipeIndex(self):
+        birdX = self.birdList[0].x
+        distanceList = [pipe.x + pipe.width - birdX for pipe in self.pipeList]  #Calculate the x distance between the bird and each pipe
+        return distanceList.index(min(i for i in distanceList if i >= 0))
+    """""""""""""""""""""""""""""
+             Test Ai
+    """""""""""""""""""""""""""""
+
+    def evalGenomes(self, genomes, config):
+        self.nets = []
+        self.birdList = []
+        self.genomes = []
+        self.run(genomes, config)
+        
+    def runNeat(self, config):
+        p = neat.Population(config)
+
+        ## Print out the statistics of the neat progress to the terminal
+        p.add_reporter(neat.StdOutReporter(True))
+        stats = neat.StatisticsReporter()
+        p.add_reporter(stats)
+
+        #Run for up to 30 generations
+        winner = p.run(self.evalGenomes, 30)
+        print('\nBest genome:\n{!s}'.format(winner))
+        with open("bestGenome.pickle", "wb") as f:
+            pickle.dump(winner, f)
+    
+    def trainAI(self):
+        self.runNeat(self.config)
+    
+    def testGenome(self, file):
+        with open(file, "rb") as f:
+            bestGenome = pickle.load(f)
+        bestNet = neat.nn.FeedForwardNetwork.create(bestGenome, self.config)
+        self.testAI(bestNet)
+    
+    def testAI(self, net):
+        run = True
+        self.birdList = []
+        self.nets = []
+        self.birdList.append(Bird(self.screen.get_width()//2, self.screen.get_height()//2))
+        self.nets.append(net)
+        self.isTraining = False
+        while(run):
+            self.clock.tick(60)
+            self.update()
+            if self.toDraw: self.draw()
+
+    
+    def setUpNeatConfig(self):
+        localDir = os.path.dirname(__file__)
+        configPath = os.path.join(localDir, "config-flappybird.txt")
+        
+        self.config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                configPath)
